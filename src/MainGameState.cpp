b28f044f7f9e1a87de0d90a4176f5b21e42be515
Alejandro García Calomarde <agc201@alu.ua.es>
2025-09-23 13:44:27 +0200

    Terminado ejercicio 2, sin probar

--- src/MainGameState.cpp
+++ src/MainGameState.cpp
@@ -9,6 +9,9 @@ MainGameState::MainGameState()
     this->player.x = 200;
     this->player.y = 200;
     this->player.vy = 0;
+
+    this->spawnTimer = 0.0f;
+    this->spawnEvery = 3.0f;
 }
 
 void MainGameState::init()
@@ -16,6 +19,9 @@ void MainGameState::init()
     this->player.x = 200;
     this->player.y = 200;
     this->player.vy = 0;
+
+    this->pipes.clear();
+    this->spawnTimer = 0.0f;
 }
 
 void MainGameState::handleInput()
@@ -28,9 +34,37 @@ void MainGameState::handleInput()
 
 void MainGameState::update(float deltaTime)
 {
-    const int gravedad = 600;
+    const float gravedad = 400.0f;
     this->player.vy += gravedad * deltaTime;
     this->player.y += this->player.vy * deltaTime;
+    
+    this->spawnTimer += deltaTime;
+    if(this->spawnTimer >= this->spawnEvery) {
+        this->spawnTimer = 0.0f;
+        int pipe_y_offset_top = GetRandomValue(PIPE_H/2, GetScreenHeight()/2);
+        float startX = GetScreenWidth();
+        Rectangle top = { 
+            startX, -pipe_y_offset_top, 
+            PIPE_W, PIPE_H 
+        };
+        Rectangle bot = {
+            startX, (PIPE_H - pipe_y_offset_top) + GetRandomValue(PIPE_H/2, GetScreenWidth()/2),
+            PIPE_W, PIPE_H 
+        };
+
+        PipePair new_pair = {top, bot};
+        this->pipes.push_back(new_pair);
+    }
+
+    for(auto& pipe : this->pipes) {
+        pipe.top.x -= PIPE_SPEED * deltaTime;
+        pipe.bot.x -= PIPE_SPEED * deltaTime;
+    }
+
+    if (!this->pipes.empty() && this->pipes.front().top.x + PIPE_W < 0) {
+        this->pipes.pop_front();
+    }
+
 }
 
 void MainGameState::render()
@@ -39,5 +73,9 @@ void MainGameState::render()
     ClearBackground(RAYWHITE);
     DrawText("Bienvenido a Flappy Bird DCA", 20, 200, 18, BLACK);
     DrawCircle(this->player.x, this->player.y, 17, RED);
+    for(const auto& pipe : this->pipes) {
+        DrawRectangleRec(pipe.top, GREEN);
+        DrawRectangleRec(pipe.bot, GREEN);
+    }
     EndDrawing();
 }
--- src/MainGameState.hpp
+++ src/MainGameState.hpp
@@ -1,10 +1,16 @@
 #pragma once
 #include <GameState.hpp>
+#include <deque>
 
 struct Bird {
     int x;
     int y;
     int vy;
+};
+
+struct PipePair { 
+    Rectangle top, bot;
+    bool scored=false;
 };
 
 class MainGameState : public GameState
@@ -25,4 +31,11 @@ class MainGameState : public GameState
     private:
         char entered_key;
         Bird player;
+        std::deque<PipePair> pipes;
+        const int PIPE_GAP = 100;
+        const int PIPE_SPEED = 100;
+        float spawnTimer;
+        float spawnEvery;
+        const int PIPE_W = 32;
+        const int PIPE_H = 320;
 };
